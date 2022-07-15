533e6c311c9964fa6e1fb6aa1c662eadfd9756b5
afonsocastro <afonsocastro@ua.pt>
2022-07-15 13:45:35 +0100

    keras working!

--- src/keras_test.py
+++ src/keras_test.py
@@ -5,168 +5,55 @@ import numpy as np
 import pandas as pd
 from tensorflow import keras
 from tensorflow.keras import layers
-
-from tensorflow.keras.layers import IntegerLookup
-from tensorflow.keras.layers import Normalization
-from tensorflow.keras.layers import StringLookup
-
-
-def encode_numerical_feature(feature, name, dataset):
-    # Create a Normalization layer for our feature
-    normalizer = Normalization()
-
-    # Prepare a Dataset that only yields our feature
-    feature_ds = dataset.map(lambda x, y: x[name])
-    feature_ds = feature_ds.map(lambda x: tf.expand_dims(x, -1))
-
-    # Learn the statistics of the data
-    normalizer.adapt(feature_ds)
-
-    # Normalize the input feature
-    encoded_feature = normalizer(feature)
-    return encoded_feature
-
-
-def encode_categorical_feature(feature, name, dataset, is_string):
-    lookup_class = StringLookup if is_string else IntegerLookup
-    # Create a lookup layer which will turn strings into integer indices
-    lookup = lookup_class(output_mode="binary")
-
-    # Prepare a Dataset that only yields our feature
-    feature_ds = dataset.map(lambda x, y: x[name])
-    feature_ds = feature_ds.map(lambda x: tf.expand_dims(x, -1))
-
-    # Learn the set of possible string values and assign them a fixed integer index
-    lookup.adapt(feature_ds)
-
-    # Turn the string input into integer indices
-    encoded_feature = lookup(feature)
-    return encoded_feature
-
-
-def dataframe_to_dataset(dataframe):
-    dataframe = dataframe.copy()
-    labels = dataframe.pop("target")
-    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
-    ds = ds.shuffle(buffer_size=len(dataframe))
-    return ds
-
+from tensorflow.keras.optimizers import Adam, SGD
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense, Dropout
 
 if __name__ == '__main__':
+    validation_split = 0.3
 
-    file_url = "http://storage.googleapis.com/download.tensorflow.org/data/heart.csv"
-    dataframe = pd.read_csv(file_url)
-
-    all_data = np.load('../data/learning_data.npy', mmap_mode=None, allow_pickle=False, fix_imports=True, encoding='ASCII')
-
-
-    print("dataframe.shape")
-    print(dataframe.shape)
-    print("type(dataframe)")
-    print(type(dataframe))
+    all_data = np.load('../data/learning_data.npy', mmap_mode=None, allow_pickle=False, fix_imports=True,
+                       encoding='ASCII')
 
     print("type(all_data)")
     print(type(all_data))
 
-    val_dataframe = dataframe.sample(frac=0.2, random_state=1337)
-    train_dataframe = dataframe.drop(val_dataframe.index)
+    # val_dataframe = dataframe.sample(frac=0.2, random_state=1337)
+    # train_dataframe = dataframe.drop(val_dataframe.index)
 
-    print("Using %d samples for training and %d for validation" % (len(train_dataframe), len(val_dataframe)))
+    validation_n = len(all_data) * validation_split
 
-    train_ds = dataframe_to_dataset(train_dataframe)
-    val_ds = dataframe_to_dataset(val_dataframe)
 
-    # for x, y in train_ds.take(1):
-    #     print("Input:", x)
-    #     print("Target:", y)
+    print("all_data[:,:-2].shape")
+    print(all_data[:, :-1].shape)
+    print("all_data[:,-1].shape")
+    print(all_data[:, -1].shape)
+    print("all_data[:, :-1]")
+    print(all_data[:, :-1])
+    print("all_data[:, -1]")
+    print(all_data[:, -1])
 
-    train_ds = train_ds.batch(32)
-    val_ds = val_ds.batch(32)
+    all_data = np.array(all_data)
+    # all_data = all_data.reshape(-1, 1)
+    #
 
-    print("type(train_ds)")
-    print(type(train_ds))
-
-    # Categorical features encoded as integers
-    sex = keras.Input(shape=(1,), name="sex", dtype="int64")
-    cp = keras.Input(shape=(1,), name="cp", dtype="int64")
-    fbs = keras.Input(shape=(1,), name="fbs", dtype="int64")
-    restecg = keras.Input(shape=(1,), name="restecg", dtype="int64")
-    exang = keras.Input(shape=(1,), name="exang", dtype="int64")
-    ca = keras.Input(shape=(1,), name="ca", dtype="int64")
-
-    # Categorical feature encoded as string
-    thal = keras.Input(shape=(1,), name="thal", dtype="string")
-
-    # Numerical features
-    age = keras.Input(shape=(1,), name="age")
-    trestbps = keras.Input(shape=(1,), name="trestbps")
-    chol = keras.Input(shape=(1,), name="chol")
-    thalach = keras.Input(shape=(1,), name="thalach")
-    oldpeak = keras.Input(shape=(1,), name="oldpeak")
-    slope = keras.Input(shape=(1,), name="slope")
-
-    all_inputs = [
-        sex,
-        cp,
-        fbs,
-        restecg,
-        exang,
-        ca,
-        thal,
-        age,
-        trestbps,
-        chol,
-        thalach,
-        oldpeak,
-        slope,
-    ]
-
-    # Integer categorical features
-    sex_encoded = encode_categorical_feature(sex, "sex", train_ds, False)
-    cp_encoded = encode_categorical_feature(cp, "cp", train_ds, False)
-    fbs_encoded = encode_categorical_feature(fbs, "fbs", train_ds, False)
-    restecg_encoded = encode_categorical_feature(restecg, "restecg", train_ds, False)
-    exang_encoded = encode_categorical_feature(exang, "exang", train_ds, False)
-    ca_encoded = encode_categorical_feature(ca, "ca", train_ds, False)
-
-    # String categorical features
-    thal_encoded = encode_categorical_feature(thal, "thal", train_ds, True)
-
-    # Numerical features
-    age_encoded = encode_numerical_feature(age, "age", train_ds)
-    trestbps_encoded = encode_numerical_feature(trestbps, "trestbps", train_ds)
-    chol_encoded = encode_numerical_feature(chol, "chol", train_ds)
-    thalach_encoded = encode_numerical_feature(thalach, "thalach", train_ds)
-    oldpeak_encoded = encode_numerical_feature(oldpeak, "oldpeak", train_ds)
-    slope_encoded = encode_numerical_feature(slope, "slope", train_ds)
-
-    all_features = layers.concatenate(
-        [
-            sex_encoded,
-            cp_encoded,
-            fbs_encoded,
-            restecg_encoded,
-            exang_encoded,
-            slope_encoded,
-            ca_encoded,
-            thal_encoded,
-            age_encoded,
-            trestbps_encoded,
-            chol_encoded,
-            thalach_encoded,
-            oldpeak_encoded,
-        ]
-    )
-    x = layers.Dense(32, activation="relu")(all_features)
-    x = layers.Dropout(0.5)(x)
-    output = layers.Dense(1, activation="sigmoid")(x)
-    model = keras.Model(all_inputs, output)
-    model.compile("adam", "binary_crossentropy", metrics=["accuracy"])
+    model = Sequential([
+        Dense(units=16, input_shape=(650,), activation='relu'),
+        Dense(units=32, activation='relu'),
+        Dropout(0.5),
+        Dense(units=4, activation='softmax')
+    ])
 
     # `rankdir='LR'` is to make the graph horizontal.
     keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
 
+    model.summary()
 
+    model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x=all_data[:, :-1], y=all_data[:, -1], validation_split=validation_split, batch_size=5, shuffle=True, epochs=50,
+              verbose=2)
 
-
-
+    print("\n")
+    print("Using %d samples for training and %d for validation" % (len(all_data) - validation_n, validation_n))
+    print("\n")
