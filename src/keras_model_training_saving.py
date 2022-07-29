db70bb17fb7f6d17ee22288ddc34109cf50d796c
afonsocastro <afonsocastro@ua.pt>
2022-07-29 12:26:33 +0100

    HyperParameters Optimization study

--- src/keras_model_training_saving.py
+++ src/keras_model_training_saving.py
@@ -1,59 +1,68 @@
 #!/usr/bin/env python3
 
-import tensorflow as tf
 import numpy as np
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.optimizers import Adam, SGD
-from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.optimizers import Adam, SGD, Nadam, RMSprop
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Activation, Dense, Dropout
 import matplotlib.pyplot as plt
+import json
+
 
 if __name__ == '__main__':
+
+    model_config = json.load(open('model_config.json'))
+
     validation_split = 0.3
 
     all_data = np.load('../data/learning_data_training.npy', mmap_mode=None, allow_pickle=False, fix_imports=True,
                        encoding='ASCII')
 
-    print("type(all_data)")
-    print(type(all_data))
-
-    # val_dataframe = dataframe.sample(frac=0.2, random_state=1337)
-    # train_dataframe = dataframe.drop(val_dataframe.index)
-
     validation_n = len(all_data) * validation_split
 
-
     all_data = np.array(all_data)
-    # all_data = all_data.reshape(-1, 1)
-    #
 
-    model = Sequential([
-        Dense(units=32, input_shape=(650,), activation='relu'),
-        Dropout(0.1),
-        Dense(units=32, activation='relu'),
-        Dropout(0.1),
-        Dense(units=32, activation='relu'),
-        Dropout(0.1),
-        # Dense(units=64, activation='relu'),
-        # Dropout(0.1),
-        Dense(units=4, activation='softmax')
-    ])
+    model = Sequential()
+
+    for layer in model_config["layers"]:
+        if layer["id"] == 0:
+            model.add(Dense(units=layer["neurons"], input_shape=(650,), activation=layer["activation"],
+                            kernel_regularizer='l1'))
+            # model.add(Dropout(layer["dropout"]))
+        elif layer["id"] == model_config["n_layers"] - 1:
+            model.add(Dense(units=layer["neurons"], activation=layer["activation"], kernel_regularizer='l1'))
+            model.add(Dropout(model_config["dropout"]))
+        else:
+            model.add(Dense(units=layer["neurons"], activation=layer["activation"], kernel_regularizer='l1'))
+            # model.add(Dropout(layer["dropout"]))
+
+    model.add(Dense(units=4, activation='softmax'))
 
     # `rankdir='LR'` is to make the graph horizontal.
-    keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
+    # keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
+    keras.utils.plot_model(model, show_shapes=True)
 
     model.summary()
 
-    model.compile(optimizer=Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    if model_config["optimizer"] == "Adam":
+        model.compile(optimizer=Adam(learning_rate=model_config["lr"]), loss=model_config["loss"],
+                      metrics=['accuracy'])
+    elif model_config["optimizer"] == "SGD":
+        model.compile(optimizer=SGD(learning_rate=model_config["lr"]), loss=model_config["loss"],
+                      metrics=['accuracy'])
+    elif model_config["optimizer"] == "Nadam":
+        model.compile(optimizer=Nadam(learning_rate=model_config["lr"]), loss=model_config["loss"],
+                      metrics=['accuracy'])
+    elif model_config["optimizer"] == "RMSprop":
+        model.compile(optimizer=RMSprop(learning_rate=model_config["lr"]), loss=model_config["loss"],
+                      metrics=['accuracy'])
 
-    callback = keras.callbacks.EarlyStopping(monitor='loss', patience=5)
+    # callback = keras.callbacks.EarlyStopping(monitor='val_loss', patience=20)
+    callback = keras.callbacks.EarlyStopping(monitor='val_loss', patience=model_config["early_stop_patience"])
 
-    fit_history = model.fit(x=all_data[:, :-1], y=all_data[:, -1], validation_split=validation_split, batch_size=64,
-                            # shuffle=True, epochs=300, verbose=2)
-                            shuffle=True, epochs=300, verbose=2, callbacks=[callback])
-                            # shuffle=True, epochs=300, verbose=2)
+    fit_history = model.fit(x=all_data[:, :-1], y=all_data[:, -1], validation_split=validation_split,
+                            batch_size=model_config["batch_size"],
+                            shuffle=True, epochs=model_config["epochs"], verbose=2, callbacks=[callback])
 
     fig = plt.figure()
 
